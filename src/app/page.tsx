305ebd99110e727803c7c7b33c154d0b82fb0f8f
Tom <33520715+wadlaw@users.noreply.github.com>
2026-04-22 12:13:58 +0100

    Uppercase the Logo call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center text-black">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <Image
-          src="/logo.png"
+          src="/Logo.png"
           alt="Standard Fire"
           className="w-1/4"
           width={80}
